04bd5c9e44cd86d842489661e862f305a32d26e6
mo3ist <50373064+mo3ist@users.noreply.github.com>
2022-01-21 18:06:00 +0200

    fix: add 'fetch' action on ViewSets

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,6 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'playlists', views.PlaylistViewSet)
 router.register(r'videos', views.VideoViewSet)
-router.register(r'channels', views.ChannelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
 from api import models, serializers
 
@@ -6,10 +9,24 @@ class PlaylistViewSet(viewsets.ModelViewSet):
 	queryset = models.Playlist.objects.all()
 	serializer_class = serializers.PlaylistSerializer
 
+	@action(methods=["post"], detail=False)
+	def fetch(self, request):
+		# Fetch the playlists of a given channel id from youtube
+		channel_id = request.data.get("channel_id", None)
+		if channel_id:
+			return Response("Hi")
+		else:
+			return Response(status=status.HTTP_400_BAD_REQUEST)
+
 class VideoViewSet(viewsets.ModelViewSet):
 	queryset = models.Video.objects.all()
 	serializer_class = serializers.VideoSerializer
 
-class ChannelViewSet(viewsets.ModelViewSet):
-	queryset = models.Channel.objects.all()
-	serializer_class = serializers.ChannelSerializer
+	@action(methods=["post"], detail=False)
+	def fetch(self, request):
+		# Fetch the videos of a given playlist from youtube
+		playlist_id = request.data.get("playlist_id", None)
+		if playlist_id:
+			return Response("Hi")
+		else:
+			return Response(status=status.HTTP_400_BAD_REQUEST)
